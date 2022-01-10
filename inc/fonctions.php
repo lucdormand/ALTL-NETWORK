4a4546096c02fde48f7fe43f4d5cb40360e0244c
fradintheo <theofradin@outlook.com>
2022-01-10 10:19:58 +0100

    maj theo

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -126,3 +126,27 @@ function generateRandomString($length = 10) {
     }
     return $randomString;
 }
+
+function verifUserConnected(){
+    if (isLogged()==false){
+        header('Location: login.php');
+    }
+}
+
+function isLogged()
+{
+    if(!empty($_SESSION['user'])) {
+        if (!empty($_SESSION['user']['id'])) {
+            if (!empty($_SESSION['user']['email'])) {
+                if (!empty($_SESSION['user']['status'])) {
+                    if (!empty($_SESSION['user']['ip'])) {
+                        if ($_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
