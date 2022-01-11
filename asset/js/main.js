6e2df26b5f167ef4e138602b7bf16e5b06dffd8b
brietalexis <alexis.briet2003@gmail.com>
2022-01-11 16:18:30 +0100

    :rocket: Dashboard

--- asset/js/main.js
+++ asset/js/main.js
@@ -26,5 +26,38 @@ sr.reveal('.wrap2',{
 
 });
 
+$(document).ready(function () {
+    const data = {
+        labels: [
+            'Red',
+            'Green',
+            'Yellow',
+            'Grey',
+            'Blue'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [11, 16, 7, 3, 14],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(75, 192, 192)',
+                'rgb(255, 205, 86)',
+                'rgb(201, 203, 207)',
+                'rgb(54, 162, 235)'
+            ]
+        }]
+    };
 
-
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            responsive: true,
+        }
+    };
+    const myChart = new Chart(
+        document.querySelector('#graph1'),
+        config
+    );
+    console.log(myChart)
+})
