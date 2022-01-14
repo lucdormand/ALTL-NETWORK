97a92d5ff33710f2ff30087df40790449c780949
fradintheo <theofradin@outlook.com>
2022-01-14 14:08:31 +0100

    :sparkles: Page contact

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,7 +7,20 @@ $(window).on('load',function() {
         controlNav: false,
     });
 })
-
+VANTA.NET({
+    el: "#js_bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3fff6c,
+    backgroundColor: 0x0,
+    maxDistance: 28.00,
+    spacing: 20.00
+})
 window.sr = ScrollReveal();
 
 sr.reveal('.footer',{
