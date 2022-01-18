1dd3da5044c855ce3d796f1b6b734080002cbe17
Louis Cherik <louis.cherik@gmail.com>
2022-01-18 13:20:03 +0100

    scripts file

---
+++ inc/scripts.php
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/scrollreveal"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.globe.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+<script src="asset/flexslider/jquery.flexslider.js"></script>
+<script src="asset/js/main.js"></script>
